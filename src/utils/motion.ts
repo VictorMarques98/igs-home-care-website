212eed44a2a83d0eef670a30af7051b942ddbda5
VictorMarques98 <victormonreal98@gmail.com>
2023-04-10 15:13:56 -0300

    feat(motion): create config file

---
+++ src/utils/motion.ts
@@ -0,0 +1,36 @@
+export const navVariants = {
+  hidden: {
+    opacity: 0,
+    y: -50,
+    transition: {
+      type: 'spring',
+      stiffness: 300,
+      damping: 140,
+    },
+  },
+  show: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      type: 'spring',
+      stiffness: 80,
+      delay: 0.3,
+    },
+  },
+};
+
+export const textVariant = (delay: number) => ({
+  hidden: {
+    y: 50,
+    opacity: 0,
+  },
+  show: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      type: 'spring',
+      duration: 1.25,
+      delay,
+    },
+  },
+});
